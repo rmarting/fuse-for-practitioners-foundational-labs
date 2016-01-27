6caedcd39f476e90d5835865c24dca110c59764b
rmarting - Roman Martin Gil <rmarting@redhat.com>
2016-01-27 23:17:48 +0100

    Use Case 3 completed!

--- usecase-3/routing/src/main/java/org/fuse/usecase/ProcessorBean.java
+++ usecase-3/routing/src/main/java/org/fuse/usecase/ProcessorBean.java
@@ -10,38 +10,24 @@ import org.slf4j.LoggerFactory;
 public class ProcessorBean {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProcessorBean.class);
-	
-    public void debug(Exchange exchange) {
-        Object body = (Object) exchange.getIn().getBody();
-        
-        // Exception
-        IllegalArgumentException exception = exchange.getException(IllegalArgumentException.class);
-        
-        // Headers
-        Map<String, Object> headers = (Map<String, Object>) exchange.getIn().getHeaders();
-        headers.put("error-code", "111");
-        headers.put("error-message", exception.getMessage());
-        
-        LOGGER.info(">> TO DEBUG >> error-code {}. error-message {}", headers.get("error-code"), headers.get("error-message"));
-        
-        // Updating Exchange headers
-        exchange.getIn().setHeaders(headers);
-        
-        LOGGER.info(">> TO DEBUG >>");
-    }
     
 	public Map<String, Object> defineNamedParameters(Exchange exchange) {
         // Headers
         Map<String, Object> headers = (Map<String, Object>) exchange.getIn().getHeaders();
 
-        // Body
+        // Error Code and Message
+        String errorCode = (String) headers.get("error-code");
+        String errorMsg = (String) headers.get("error-message");
+        // Message
         String message = exchange.getIn().getBody(String.class);
+        
+        LOGGER.info("Preparing data to insert into DB. Error: {}-{}", errorCode, errorMsg);
 
         // Preparing data to save in DB
 		Map<String, Object> map = new HashMap<String, Object>();
 		
-		map.put("ERROR_CODE", headers.get("error-code"));
-		map.put("ERROR_MESSAGE", headers.get("error-message"));
+		map.put("ERROR_CODE", errorCode);
+		map.put("ERROR_MESSAGE", errorMsg);
 		map.put("MESSAGE", message);
 		map.put("STATUS", "ERROR");
 		
