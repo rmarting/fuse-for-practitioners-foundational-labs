7dbd785f527a084d12314fc12e582ca276234b7a
rmarting - Roman Martin Gil <rmarting@redhat.com>
2016-02-01 12:37:22 +0100

    Latest changes, but not tested

--- usecase-4/testing/src/test/java/org/fuse/usecase/LDAPActiveMQTest.java
+++ usecase-4/testing/src/test/java/org/fuse/usecase/LDAPActiveMQTest.java
@@ -20,36 +20,66 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
-@RunWith(FrameworkRunner.class) @CreateLdapServer(transports = {
-        @CreateTransport(protocol = "LDAP", port = 1024) }) @ApplyLdifFiles("org/fuse/usecase/activemq.ldif") public class LDAPActiveMQTest
-        extends AbstractLdapTestUnit {
+@RunWith(FrameworkRunner.class)
+@CreateLdapServer(transports = { @CreateTransport(protocol = "LDAP", port = 1024) })
+@ApplyLdifFiles("org/fuse/usecase/activemq.ldif")
+public class LDAPActiveMQTest extends AbstractLdapTestUnit {
+	
+	// http://ibookmate.blogspot.be/2010/07/jaas-loginmodule-for-activemq.html
 
-    public BrokerService broker;
-    public static LdapServer ldapServer;
+	public BrokerService broker;
+	public static LdapServer ldapServer;
 
-    @Before
-    public void setup() throws Exception {
-        System.setProperty("ldapPort", String.valueOf(getLdapServer().getPort()));
-        broker = BrokerFactory.createBroker("xbean:org/fuse/usecase/activemq-broker.xml");
-        broker.start();
-        broker.waitUntilStarted();
-    }
+	@Before
+	public void setup() throws Exception {
+		System.setProperty("ldapPort",
+				String.valueOf(getLdapServer().getPort()));
+		broker = BrokerFactory
+				.createBroker("xbean:org/fuse/usecase/activemq-broker.xml");
+		broker.start();
+		broker.waitUntilStarted();
+	}
 
-    @After public void shutdown() throws Exception {
-        broker.stop();
-        broker.waitUntilStopped();
-    }
+	@After
+	public void shutdown() throws Exception {
+		broker.stop();
+		broker.waitUntilStopped();
+	}
 
-    @Test public void testFailCreateSessionNotEnoughRight() throws Exception {
+	@Test
+	public void testFailCreateSessionNotEnoughRight() throws Exception {
 
-    }
+	}
 
-    @Test public void testCreateQueuePublishConsume() throws Exception {
+	@Test
+	public void testCreateQueuePublishConsume() throws Exception {
+		try {
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616"); // apparently the vm part is all i need
+            Connection connection = connectionFactory.createConnection("jdoe", "sunflower"); 
+            connection.start();
+            
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue("TEST.FOO");
+            MessageProducer producer = session.createProducer(destination);
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+            String text = "Hello world! From: " + Thread.currentThread().getName() + " : " + this.hashCode();
+            TextMessage message = session.createTextMessage(text);
+            System.out.println("Sent message: "+ message.hashCode() + " : " + Thread.currentThread().getName());
+            producer.send(message);
+            session.close();
+            connection.close();
+        }
+        catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+	
+	
+	}
 
-    }
+	@Test
+	public void testFailCreateQueuePublishConsume() throws Exception {
 
-    @Test public void testFailCreateQueuePublishConsume() throws Exception {
-
-    }
+	}
 
 }
